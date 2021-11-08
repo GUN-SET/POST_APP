dbd5cba4be1a6d43bf58b82668adcc0b1db18653
GUN-SET <androysol@gmail.com>
2021-11-08 18:11:04 +0200

    Second commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это приложение создано Андреем Соловьёвым для практики
+        </h1>
+    );
+};
+
+export default About;
